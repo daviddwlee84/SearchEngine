d903aa57150a61218ae2ec12700adb9197b83e68
David Lee <daviddwlee84@gmail.com>
2020-08-21 15:50:14 +0800

    keyword extract with weight

--- mining/keyword_extractor.py
+++ mining/keyword_extractor.py
@@ -1,3 +1,4 @@
+from typing import List, Tuple
 from jieba import analyse
 import jieba
 import os
@@ -28,22 +29,29 @@ class KeywordExtractor(object):
         self.default_method = default_method
         # TFIDF()
 
-    def get_topk_keywords(self, text: str, topk: int, method: str = None):
+    def get_topk_keywords(self, text: str, topk: int, method: str = None) -> List[Tuple[str, int]]:
         """
         TODO: maybe add a mix mode?!
+
+        TODO: make withWeight an argument?
+
+        Return [(keyword, weight), ...]
         """
         if not method:
             method = self.default_method
 
         if method == 'jieba.textrank':
-            result = analyse.textrank(text, topK=topk)
+            result = analyse.textrank(text, topK=topk, withWeight=True)
         elif method == 'jieba.extract_tags':
-            result = analyse.extract_tags(text, topK=topk)
+            result = analyse.extract_tags(text, topK=topk, withWeight=True)
         elif method == 'jieba.tfidf':
-            result = analyse.tfidf(text, topK=topk)
+            result = analyse.tfidf(text, topK=topk, withWeight=True)
 
         return result
 
 
 if __name__ == "__main__":
-    KeywordExtractor()
+    extractor = KeywordExtractor()
+    for method in ['jieba.textrank', 'jieba.extract_tags', 'jieba.tfidf']:
+        print(extractor.get_topk_keywords(
+            '华人运通与微软在麦当劳讨论Tiktok收购的事情', 5, method))
